34e1d125905a8e434b3bc4a768160c1a6a6d3069
businesssuccess <159882838+businesssuccess@users.noreply.github.com>
2024-10-28 18:25:17 +1100

    Add files via upload

---
+++ cpFrameworkBadge-1-0.js
@@ -0,0 +1,7 @@
+<script>
+  $('a[href="/poweredby_badge"] svg').remove();
+
+  $('<img style="position:fixed; right:0; bottom:0;" src="https://statics.myclickfunnels.com/workspace/jWlrDR/image/5299355/file/170909299364c3d0a02c9d34db65deff.svg" width="170">').appendTo( $('a[href="/poweredby_badge"]') );
+
+  $('a[href="/poweredby_badge"]').attr('href', 'https://www.connectingpieces.com/cp-frameworks');
+</script>
