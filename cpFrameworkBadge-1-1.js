6ce740ba4751d378bdb84da1813da0a9c1eb517a
businesssuccess <159882838+businesssuccess@users.noreply.github.com>
2024-10-28 18:55:37 +1100

    Update cpFrameworkBadge-1-1.js

--- cpFrameworkBadge-1-1.js
+++ cpFrameworkBadge-1-1.js
@@ -1,5 +1,5 @@
   $('a[href="/poweredby_badge"] svg').remove();
 
-  $('<img style="position:fixed; right:0; bottom:0;" src="https://statics.myclickfunnels.com/workspace/jWlrDR/image/5299355/file/170909299364c3d0a02c9d34db65deff.svg" width="170">').appendTo( $('a[href="/poweredby_badge"]') );
+  $('<img style="position:fixed; right:0; bottom:0;" src="https://statics.myclickfunnels.com/workspace/jWlrDR/image/5299889/file/86e4080da506cd519709617663bbc133.svg" width="170">').appendTo( $('a[href="/poweredby_badge"]') );
 
   $('a[href="/poweredby_badge"]').attr('href', 'https://www.connectingpieces.com/cp-frameworks');
