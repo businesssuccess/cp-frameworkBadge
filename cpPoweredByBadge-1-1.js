93542e9943da67cc7d5302564123ca1eecc95470
businesssuccess <159882838+businesssuccess@users.noreply.github.com>
2025-01-23 16:39:20 +1100

    Add files via upload

---
+++ cpPoweredByBadge-1-1.js
@@ -0,0 +1,5 @@
+  $('a[href="/poweredby_badge"] svg').remove();
+
+  $('<img style="position:fixed; right:0; bottom:0;" src="https://statics.myclickfunnels.com/workspace/jWlrDR/image/5299889/file/86e4080da506cd519709617663bbc133.svg" width="170">').appendTo( $('a[href="/poweredby_badge"]') );
+
+  $('a[href="/poweredby_badge"]').attr('href', 'https://www.connectingpieces.com/cp-frameworks');
