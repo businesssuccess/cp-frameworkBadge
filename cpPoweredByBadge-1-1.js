931c404c0934adde63068d8c10a41412202869df
businesssuccess <159882838+businesssuccess@users.noreply.github.com>
2025-01-23 16:44:30 +1100

    Update cpPoweredByBadge-1-1.js

--- cpPoweredByBadge-1-1.js
+++ cpPoweredByBadge-1-1.js
@@ -1,4 +1,4 @@
-  $('a[href="/poweredby_badge"] svg').remove();
+$('a[href="/poweredby_badge"] svg').remove();
 
   $('<img style="position:fixed; right:0; bottom:0;" src="https://statics.myclickfunnels.com/workspace/jWlrDR/image/5299889/file/86e4080da506cd519709617663bbc133.svg" width="170">').appendTo( $('a[href="/poweredby_badge"]') );
 
